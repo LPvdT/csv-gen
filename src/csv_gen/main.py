3301b6f106fd95f6a8f9ee07f43d920ecc6c5330
Laurens van der Tas <laurens.vandertas@gmail.com>
2025-09-13 12:31:58 +0200

    update main.py

--- src/csv_gen/main.py
+++ src/csv_gen/main.py
@@ -7,7 +7,6 @@ from typing import Final
 from loguru import logger
 
 from csv_gen.utils.np import main_np
-from csv_gen.utils.python import main_py
 
 # Configuration
 FILENAME: Final[str] = "bigfile.csv"
@@ -23,23 +22,16 @@ if __name__ == "__main__":
         "filename": FILENAME,
         "header": ["id", "name", "value1", "value2", "value3"],
         "target_size": 25 * 1024**3,  # 25 GB
-        "rows_per_chunk": 100_000,
     }
 
     args_2 = {
         "filename": FILENAME,
         "header": ["id", "name", "value1", "value2", "value3"],
         "target_size": 25 * 1024**3,  # 25 GB
-        "rows_per_chunk": 2_500_000,
     }
 
     np_algo_config_1 = functools.partial(main_np, **args)
     np_algo_config_2 = functools.partial(main_np, **args_2)
-    py_algo_config = functools.partial(main_py, **args)
 
     time_np = timeit.timeit(np_algo_config_2, number=1)
     logger.info(f"NumPy: {time_np:.2f} s")
-
-    # NOTE: Currently not testing Python algorithm
-    # time_py = timeit.timeit(py_algo_config, number=1)
-    # logger.info(f"Python: {time_py:.2f} s")
