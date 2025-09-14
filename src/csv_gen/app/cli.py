eb157c91eb330f22bd48e56559262495c2df597b
Laurens van der Tas <laurens.vandertas@gmail.com>
2025-09-14 12:41:22 +0200

    update cli.py

--- src/csv_gen/app/cli.py
+++ src/csv_gen/app/cli.py
@@ -31,7 +31,8 @@ def generate(
     Args:
         file_name (str, optional): The name of the generated file
         file_size_gb (int, optional): The size of the generated file in bytes _(default is a gigabyte: `1 * 1024**3`)_
-        cpus (int | None, optional): The number of CPUs to use for generation _(uses all cores when None)_
+        cpus (int | None, optional): The number of CPUs to use for generation _(uses all cores when `None`)_
+        algorithm (Literal["faker", "numpy"], optional): The algorithm to use, either `faker` or `numpy`
     """
 
     main_csv(
