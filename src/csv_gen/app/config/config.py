86ff88a646c48f68ab982f18c15c4add5b798ee3
Laurens van der Tas <laurens.vandertas@gmail.com>
2025-09-14 10:52:16 +0200

    update config.py

--- src/csv_gen/app/config/config.py
+++ src/csv_gen/app/config/config.py
@@ -1,12 +1,17 @@
 from __future__ import annotations
 
 import functools
+from pathlib import Path
 
 from pydantic import Field
 from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
+    LOG_PATH: Path = Field(
+        default=Path(__file__).parents[2] / "logs",
+        description="Path to store log files",
+    )
     FILENAME: str = Field(default="generated.csv", description="CSV file name")
     DEFAULT_HEADERS: list[str] = Field(
         default=[
