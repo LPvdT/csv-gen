6f3c32785015f67e324510cf377559d9c3725593
Laurens van der Tas <laurens.vandertas@gmail.com>
2025-09-14 10:29:53 +0200

    update faker_utils.py

--- src/csv_gen/app/algorithms/utils/faker_utils.py
+++ src/csv_gen/app/algorithms/utils/faker_utils.py
@@ -58,7 +58,24 @@ def generate_chunk_faker(start_id: int, count: int, tmp_file: str) -> str:
 
 
 def generate_batch_faker(start_id: int, count: int) -> bytes:
+    """
+    Generate a batch of CSV data using Faker.
+
+    Parameters
+    ----------
+    start_id : int
+        The starting ID for the batch.
+    count : int
+        The number of rows to generate in the batch.
+
+    Returns
+    -------
+    bytes
+        A bytes object containing rows of CSV data.
+    """
+
     rows = [
         build_row_faker(row_id) for row_id in range(start_id, start_id + count)
     ]
+
     return "".join(rows).encode("utf-8")
