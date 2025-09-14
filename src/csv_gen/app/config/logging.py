574235e3498d0a972d76bca6a65630a7c208b2e4
Laurens van der Tas <laurens.vandertas@gmail.com>
2025-09-14 10:52:45 +0200

    update logging.py

--- src/csv_gen/app/config/logging.py
+++ src/csv_gen/app/config/logging.py
@@ -3,7 +3,12 @@ import sys
 
 from loguru import logger
 
+from csv_gen.app.config.config import get_settings
+
 # Setup logging
+settings = get_settings()
+settings.LOG_PATH.mkdir(exist_ok=True, parents=True)
+
 default_options = {
     "level": logging.DEBUG,
     "enqueue": True,
@@ -12,7 +17,7 @@ default_options = {
 logger.remove()
 logger.add(sys.stderr, **default_options)
 logger.add(
-    "{time:YYYY-MM-DD_HH-mm-ss}.log",
+    settings.LOG_PATH / "{time:YYYY-MM-DD_HH-mm-ss}.log",
     rotation="100 KB",
     retention="5 days",
     **default_options,
