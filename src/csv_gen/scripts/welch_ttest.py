a2c328b7a9850f1b88d5c783ea56f153072097cd
Laurens van der Tas <laurens.vandertas@gmail.com>
2025-10-18 12:55:24 +0200

    update welch_ttest.py

--- src/csv_gen/scripts/welch_ttest.py
+++ src/csv_gen/scripts/welch_ttest.py
@@ -8,13 +8,10 @@
 # ]
 # ///
 
-"""This script performs Welch's t-test on a JSON export file with two
-benchmark results to test whether or not the two distributions are
-the same."""
-
 import argparse
 import json
 import logging
+import pathlib
 import sys
 
 from loguru import logger
@@ -23,30 +20,28 @@ from scipy import stats
 logger.remove()
 logger.add(sys.stderr, level=logging.DEBUG)
 
-parser = argparse.ArgumentParser(description=__doc__)
-parser.add_argument("file", help="JSON file with two benchmark results")
-args = parser.parse_args()
 
-with open(args.file, encoding="utf-8") as f:
-    results = json.load(f)["results"]
+def main() -> None:
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("file", help="JSON file with two benchmark results")
+    args = parser.parse_args()
 
-if len(results) != 2:
-    print("The input file has to contain exactly two benchmarks")
-    sys.exit(1)
+    with pathlib.Path(args.file).open(encoding="utf-8") as f:
+        results = json.load(f)["results"]
 
-a, b = (x["command"] for x in results[:2])
-X, Y = (x["times"] for x in results[:2])
+    if len(results) != 2:  # noqa
+        sys.exit(1)
 
-print(f"Command 1: {a}")
-print(f"Command 2: {b}\n")
+    _a, _b = (x["command"] for x in results[:2])
+    X, Y = (x["times"] for x in results[:2])
 
-t, p = stats.ttest_ind(X, Y, equal_var=False)
-th = 0.05
-dispose = p < th
-print(f"t = {t:.3}, p = {p:.3}")
-print()
+    _t, p = stats.ttest_ind(X, Y, equal_var=False)
+    th = 0.05
+    dispose = p < th  # type: ignore
 
-if dispose:
-    print(f"There is a difference between the two benchmarks (p < {th}).")
-else:
-    print(f"The two benchmarks are almost the same (p >= {th}).")
+    if dispose:
+        pass
+
+
+if __name__ == "__main__":
+    main()
