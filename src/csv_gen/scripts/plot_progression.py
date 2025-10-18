2ba37c25bac19d4af5f1724357e2ead6df561a19
Laurens van der Tas <laurens.vandertas@gmail.com>
2025-10-18 12:57:58 +0200

    update plot_progression.py

--- src/csv_gen/scripts/plot_progression.py
+++ src/csv_gen/scripts/plot_progression.py
@@ -10,14 +10,10 @@
 # ]
 # ///
 
-"""This program shows `hyperfine` benchmark results in a sequential way
-in order to debug possible background interference, caching effects,
-thermal throttling and similar effects.
-"""
-
 import argparse
 import json
 import logging
+import pathlib
 import sys
 
 import matplotlib.pyplot as plt
@@ -28,7 +24,7 @@ logger.remove()
 logger.add(sys.stderr, level=logging.DEBUG)
 
 
-def moving_average(times, num_runs):
+def moving_average(times: np.ndarray, num_runs: int) -> np.ndarray:
     times_padded = np.pad(
         times, (num_runs // 2, num_runs - 1 - num_runs // 2), mode="edge"
     )
@@ -36,62 +32,68 @@ def moving_average(times, num_runs):
     return np.convolve(times_padded, kernel, mode="valid")
 
 
-parser = argparse.ArgumentParser(description=__doc__)
-parser.add_argument("file", help="JSON file with benchmark results")
-parser.add_argument("--title", help="Plot Title")
-parser.add_argument("-o", "--output", help="Save image to the given filename.")
-parser.add_argument(
-    "-w",
-    "--moving-average-width",
-    type=int,
-    metavar="num_runs",
-    help="Width of the moving-average window (default: N/5)",
-)
-parser.add_argument(
-    "--no-moving-average",
-    action="store_true",
-    help="Do not show moving average curve",
-)
+def main() -> None:
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("file", help="JSON file with benchmark results")
+    parser.add_argument("--title", help="Plot Title")
+    parser.add_argument(
+        "-o", "--output", help="Save image to the given filename."
+    )
+    parser.add_argument(
+        "-w",
+        "--moving-average-width",
+        type=int,
+        metavar="num_runs",
+        help="Width of the moving-average window (default: N/5)",
+    )
+    parser.add_argument(
+        "--no-moving-average",
+        action="store_true",
+        help="Do not show moving average curve",
+    )
+
+    args = parser.parse_args()
+
+    with pathlib.Path(args.file).open(encoding="utf-8") as f:
+        results = json.load(f)["results"]
+
+    for result in results:
+        _label = result["command"]
+        times = result["times"]
+        num = len(times)
+        nums = range(num)
+
+        plt.scatter(x=nums, y=times, marker=".")
+        plt.ylim([0, None])
+        plt.xlim([-1, num])
+
+        if not args.no_moving_average:
+            moving_average_width = (
+                num // 5
+                if args.moving_average_width is None
+                else args.moving_average_width
+            )
+
+            average = moving_average(times, moving_average_width)
+            plt.plot(nums, average, "-")
+
+    if args.title:
+        plt.title(args.title)
+
+    legend = []
+    for result in results:
+        legend.append(result["command"])
+        if not args.no_moving_average:
+            legend.append("moving average")
+    plt.legend(legend)
+
+    plt.ylabel("Time [s]")
+
+    if args.output:
+        plt.savefig(args.output)
+    else:
+        plt.show()
 
 
-args = parser.parse_args()
-
-with open(args.file) as f:
-    results = json.load(f)["results"]
-
-for result in results:
-    label = result["command"]
-    times = result["times"]
-    num = len(times)
-    nums = range(num)
-
-    plt.scatter(x=nums, y=times, marker=".")
-    plt.ylim([0, None])
-    plt.xlim([-1, num])
-
-    if not args.no_moving_average:
-        moving_average_width = (
-            num // 5
-            if args.moving_average_width is None
-            else args.moving_average_width
-        )
-
-        average = moving_average(times, moving_average_width)
-        plt.plot(nums, average, "-")
-
-if args.title:
-    plt.title(args.title)
-
-legend = []
-for result in results:
-    legend.append(result["command"])
-    if not args.no_moving_average:
-        legend.append("moving average")
-plt.legend(legend)
-
-plt.ylabel("Time [s]")
-
-if args.output:
-    plt.savefig(args.output)
-else:
-    plt.show()
+if __name__ == "__main__":
+    main()
